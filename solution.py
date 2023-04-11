7d22687a509c92f2a8a8366cfd29e940f575927e
Andrey Solot <114979763+AndreySolot@users.noreply.github.com>
2023-04-11 14:28:58 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import numpy as np
+import scipy.stats as sps
+
+chat_id = 163596104 # Ваш chat ID, не меняйте название переменной
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(x: np.array, y: np.array) -> bool:
+    
+    pvalue = sps.permutation_test((x, y), lambda x, y, axis: np.mean(x, axis=axis) - np.mean(y, axis=axis), 
+                 vectorized=True, 
+                 n_resamples=1000,
+                 alternative='greater').pvalue 
+    alpha = 0.06
+    return pvalue < alpha
