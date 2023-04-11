3c04e9b46f08cf06ef80a7be97a4536ac0124ec7
Andrey Solot <114979763+AndreySolot@users.noreply.github.com>
2023-04-11 19:42:32 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,15 +1,15 @@
 import pandas as pd
 import numpy as np
 import scipy.stats as sps
+from scipy.stats import mannwhitneyu
 
 chat_id = 163596104 # Ваш chat ID, не меняйте название переменной
 
 
 def solution(x: np.array, y: np.array) -> bool:
     
-    pvalue = sps.permutation_test((x, y), lambda x, y, axis: np.mean(x, axis=axis) - np.mean(y, axis=axis), 
-                 vectorized=True, 
-                 n_resamples=1000,
-                 alternative='greater').pvalue 
+    pvalue = mannwhitneyu(x, y, alternative=alternative).pvalue
+    
     alpha = 0.06
     return pvalue < alpha
+
